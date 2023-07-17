6f6659efbd09d23fd3acfbe62c7eb78445dacb4e
Tanjir-Hasan <ihasan.tanjir@gmail.com>
2023-07-17 16:30:35 +0600

    banner uodated

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,59 +1,33 @@
 import { useContext } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 import { ThemeContext } from '../../../providers/ThemeProvider';
 
 const Banner = () => {
 
-    const { theme, toggleTheme } = useContext(ThemeContext);
+    const { theme } = useContext(ThemeContext);
 
     return (
+        <div className={`my-component ${theme === 'dark' ? 'dark' : ''}`}>
 
-        <>
-
-            <div className={`my-component ${theme === 'dark' ? 'dark' : ''}`}>
-
-            <div className="carousel w-full">
-                    <div id="item1" className="item1 carousel-item w-full lg:flex items-center">
-                        <div className='space-y-2 lg:px-20 px-5 py-3'>
-                            <h4 className='text-2xl font-[Cinzel] lg:text-center sm:text-right'>
-                                Unlock Your Linguistic Potential
-                            </h4>
-                            <p className='font-[Inconsolata] lg:text-center sm:text-right'>At Language Mastery, embark on an immersive journey into the world of languages. Our dynamic language school offers a diverse range of courses tailored to suit every learner's needs. Whether you're a beginner or advanced, our experienced instructors will guide you to fluency, empowering you to communicate confidently in any language. Don't be afraid. Let's do it together.</p>
-                        </div>
-                    </div>
+            <Carousel autoPlay>
+                <div>
+                    <img src="https://i.ibb.co/rffKGDN/photo-1546410531-bb4caa6b424d.jpg" />
+                    <p className="legend font-[Inconsolata]">Indulge your love for learning at our vibrant language school. Discover the magic of acquiring new languages through interactive classes, engaging activities, and personalized guidance. Our dedicated team of educators will ignite your curiosity and fuel your desire to master languages, creating an inspiring and supportive environment for your educational journey.</p>
                 </div>
 
-                <div className="carousel w-full" >
-                    <div id="item1" className="carousel-item w-full lg:flex items-center">
-                        <img src="https://i.ibb.co/my6NdPZ/photo-1555431189-0fabf2667795.jpg" className="w-full" />
-                    </div>
-                    <div id="item2" className="carousel-item w-full">
-                        <img src="https://i.ibb.co/rffKGDN/photo-1546410531-bb4caa6b424d.jpg" className="w-full" />
-                    </div>
-                    <div id="item3" className="carousel-item w-full">
-                        <img src="https://i.ibb.co/r5Wb6hh/photo-1568047571827-8c46fe611345.jpg" className="w-full" />
-                    </div>
+                <div>
+                    <img src="https://i.ibb.co/my6NdPZ/photo-1555431189-0fabf2667795.jpg" className='h-1/2' />
+                    <p className="legend font-[Inconsolata]">Indulge your love for learning at our vibrant language school. Discover the magic of acquiring new languages through interactive classes, engaging activities, and personalized guidance. Our dedicated team of educators will ignite your curiosity and fuel your desire to master languages, creating an inspiring and supportive environment for your educational journey. At Language Mastery, embark on an immersive journey into the world of languages. Our dynamic language school offers a diverse range of courses tailored to suit every learner's needs. Whether you're a beginner or advanced, our experienced instructors will guide you to fluency, empowering you to communicate confidently in any language. Don't be afraid. Let's do it together.</p>
                 </div>
 
-                <div className="carousel w-full">
-                    <div id="item1" className="item1 carousel-item w-full lg:flex items-center">
-                        <div className='space-y-2 lg:px-20 px-5 py-3'>
-                            <h4 className='text-2xl font-[Cinzel] lg:text-center sm:text-right'>
-                                Explore the Joy of Language Acquisition, Open the Doors to Multilingual Proficiency
-                            </h4>
-                            <p className='font-[Inconsolata] lg:text-center sm:text-right'>Indulge your love for learning at our vibrant language school. Discover the magic of acquiring new languages through interactive classes, engaging activities, and personalized guidance. Our dedicated team of educators will ignite your curiosity and fuel your desire to master languages, creating an inspiring and supportive environment for your educational journey.</p>
-                        </div>
-                    </div>
+                <div>
+                    <img src="https://i.ibb.co/r5Wb6hh/photo-1568047571827-8c46fe611345.jpg" />
+                    <p className="legend font-[Inconsolata]">At Language Mastery, embark on an immersive journey into the world of languages. Our dynamic language school offers a diverse range of courses tailored to suit every learner's needs. Whether you're a beginner or advanced, our experienced instructors will guide you to fluency, empowering you to communicate confidently in any language. Don't be afraid. Let's do it together.</p>
                 </div>
+            </Carousel>
 
-                <div className="flex justify-center w-full py-2 gap-2">
-                    <a href="#item1" className="btn btn-xs">1</a>
-                    <a href="#item2" className="btn btn-xs">2</a>
-                    <a href="#item3" className="btn btn-xs">3</a>
-                </div>
-            </div>
-
-        </>
+        </div>
     );
 };
 
